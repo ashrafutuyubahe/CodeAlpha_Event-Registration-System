efd696fc1902bf02f2f09497e5e34e32b9613287
ashraftuyubahe <ashrafutyubahe001u@gmail.com>
2025-01-21 19:14:06 +0200

    working on event management

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -10,32 +10,32 @@ export class EventService {
     private readonly eventRepository: Repository<Event>,
   ) {}
 
-  // Get all events
+
   async getAllEvents(): Promise<Event[]> {
     return await this.eventRepository.find();
   }
 
-  // Get a single event by ID
+
   async getEvent(id: number): Promise<Event> {
     return await this.eventRepository.findOneOrFail({
       where: { eventId: id },
     });
   }
 
-  // Create a new event
+
   async createEvent(eventData: Partial<Event>): Promise<Event> {
     const event = this.eventRepository.create(eventData);
     return await this.eventRepository.save(event);
   }
 
-  // Update an event
+  
   async updateEvent(id: number, eventData: Partial<Event>): Promise<Event> {
     const event = await this.getEvent(id);
     Object.assign(event, eventData);
     return await this.eventRepository.save(event);
   }
 
-  // Delete an event
+  
   async deleteEvent(id: number): Promise<void> {
     const event = await this.getEvent(id);
     await this.eventRepository.remove(event);
